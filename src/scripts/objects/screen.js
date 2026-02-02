1cc9f02fb46b2411ba90ac073f7f91da620e5970
daniel-futenma <ziki.luter20013@gmail.com>
2026-02-02 21:28:04 +0900

    correcao de erros

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,26 +1,29 @@
 const screen = {
     userProfile: document.querySelector('.profile-data'),
     renderUser(user) {
-        this.userProfile.innerHTML = `<div class="info"><img src="${user.avatarUrl}" alt="Foto de perfil do usuario" />
-                                        <div class="data">
-                                            <h1>${user.name ?? 'nao possui nome cadastrado'}</h1>
-                                            <p>${user.bio ?? 'nao possui bio cadastrado'} </p>
-                                            <p>
-                                               👥 Followers: ${user.followers} |
-                                              ➕ Following: ${user.following}
-                                             </p>
-                                        </div>
-                                    </div>`
+        this.userProfile.innerHTML = `<div class="info">
+                                           <img src="${user.avatarUrl}" alt="Foto de perfil do usuario" >
+                                            <div class="data">
+                                                   <h1>${user.name ?? 'nao possui nome cadastrado'}</h1>
+                                                   <p>${user.bio ?? 'nao possui bio cadastrado'} </p>
+                                                   <p>
+                                                    👥 Followers: ${user.followers} |
+                                                    ➕ Following: ${user.following}
+                                                   </p>
+                                            </div>
+                                        </div>`
 
         let repositoriesItens = ''
-        user.repositories.forEach(repo => {repositoriesItens += `<li><a href="${repo.html_url}" target ="_blank">${repo.name}</a>
+        user.repositories.forEach(repo => {
+            repositoriesItens += `<li><a href="${repo.html_url}" target ="_blank">${repo.name}</a>
                                                                    <div class="repo-info">
                                                                      <span class="repo-badge stars"> ⭐${repo.stargazers_count}</pan>
                                                                      <span class="repo=-badge forks"> 🍴 ${repo.forks_count}</pan>
                                                                      <span class="repo=-badge watchers"> 👀 ${repo.watchers_count}</pan>
                                                                      <span class="repo=-badge language"> 💻 ${repo.language ?? 'Não informado'}</pan>
                                                                     </div>
-                                                                </li>`})
+                                    </li>`})
+
         if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class = "repositories section">
                                               <h2> Repositorios</h2>
