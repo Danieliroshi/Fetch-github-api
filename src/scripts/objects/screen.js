4789dc01e92e4aaa1b1fd65c39df611f632c5fcd
daniel-futenma <ziki.luter20013@gmail.com>
2026-02-02 20:45:12 +0900

    up  followers

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -4,24 +4,34 @@ const screen = {
         this.userProfile.innerHTML = `<div class="info"><img src="${user.avatarUrl}" alt="Foto de perfil do usuario" />
                            <div class="data">
                              <h1>${user.name ?? 'nao possui nome cadastrado'}</h1>
-                             <p>${user.bio ?? 'nao possui bio cadastrado'}</p>
-                          </div>
-                        </div>`
+                             <p>${user.bio ?? 'nao possui bio cadastrado'} </p>
+                              <p>
+                               👥 Followers: ${user.followers} |
+                               ➕ Following: ${user.following}
+                               </p>
+                           </div>`
 
         let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="#${repo.html_url}" target ="_blank">${repo.name}</a></li>
+        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target ="_blank">${repo.name}</a>
+                                                                   <div class="repo-info">
+                                                                     <span class="repo-badge stars"> ⭐${repo.stargazers_count}</pan>
+                                                                     <span class="repo=-badge forks"> 🍴 ${repo.forks_count}</pan>
+                                                                     <span class="repo=-badge watchers"> 👀 ${repo.watchers_count}</pan>
+                                                                     <span class="repo=-badge language"> 💻 ${repo.language ?? 'Não informado'}</pan>
+                                                                    </div>
+                                                                </li>
             `)
         if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class = "repositories section">
                                               <h2> Repositorios</h2>
                                               <ul>${repositoriesItens}</ul>
-                                             </div`
+                                             </div>`
         }
 
 
     },
-    renderNotFound(){
-        this.userProfile.innerHTML ="<h3>Usuario nao encontrado</h3> "
+    renderNotFound() {
+        this.userProfile.innerHTML = "<h3>Usuario nao encontrado</h3> "
     }
 
 }
--- src/scripts/objects/user.js
+++ src/scripts/objects/user.js
@@ -6,11 +6,16 @@
     bio:'',
     userName:'',
     repositories: [],
+    followers:0,
+    following:0,
+
     setInfo(gitHubUser){
         this.avatarUrl = gitHubUser.avatar_url
         this.name = gitHubUser.name
         this.bio = gitHubUser.bio
         this.userName = gitHubUser.login 
+        this.followers = gitHubUser.followers
+        this.following = gitHubUser.following
     },
     setRepositories(repositories){
         this.repositories = repositories
